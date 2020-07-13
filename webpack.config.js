49db0d39610e295ff2afc9479066b41d4a657b43
Misha Memus <fedarovichmikhail@gmail.com>
2020-07-13 05:43:34 +0300

    added cash js files and install babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
   entry: './src/script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    new WebpackMd5Hash()
+  ]
 }
